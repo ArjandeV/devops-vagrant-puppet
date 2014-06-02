82d138601a7862bf6c11294cd4eb546221429ab8
Stéphane Thibault <thibault.stephane@gmail.com>
2014-06-02 11:55:16 -0400

    + Composer global install

--- puppet/modules/local/assets/manifests/composer.pp
+++ puppet/modules/local/assets/manifests/composer.pp
@@ -5,18 +5,40 @@
 class assets::composer {
     wget::fetch { 'composer-install':
         source => 'http://getcomposer.org/composer.phar',
-        destination => '/vagrant/puppet/.tmp/cache/composer.phar',
+        destination => '/tmp/composer.phar',
+        cache_dir => '/var/cache/wget',
     }
 
     file { '/usr/local/bin/composer':
         ensure => 'present',
-        source => '/vagrant/puppet/.tmp/cache/composer.phar',
+        source => '/tmp/composer.phar',
         mode => '+x',
         require => Wget::Fetch['composer-install'],
     }
 
-    file { '~/.composer':
+    file { '/home/vagrant/.composer':
         ensure => 'directory',
+        owner  => 'vagrant',
+        group  => 'vagrant',
+        require => File['/usr/local/bin/composer'],
+    }
+
+    file { '/home/vagrant/.composer/composer.json':
+        ensure => 'present',
+        source => '/vagrant/puppet/files/composer.json',
+        owner  => 'vagrant',
+        group  => 'vagrant',
+        require => File['/home/vagrant/.composer'],
+    }
+
+    exec { 'composer global install':
+        command => '/usr/local/bin/composer global install',
+        environment => ['COMPOSER_HOME=/home/vagrant/.composer'],
+        require => [
+            File['/usr/local/bin/composer'],
+            File['/home/vagrant/.composer/composer.json'],
+        ],
+        logoutput => true,
     }
 
 }
